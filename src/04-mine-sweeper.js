44b8254fd4b656ef6817b7638ca3a8bf3d50cf53
Nazkoro <50379512+Nazkoro@users.noreply.github.com>
2021-04-12 23:36:25 +0300

    feat: add code-4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,49 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+
+function minesweeper(arr) {
+  const array = arr;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[0].length; j++) {
+      if (array[i][j] === true) {
+        array[i][j] = 9;
+      } else array[i][j] = 0;
+    }
+  }
+
+  function plusOne(h, w) {
+    if (h >= 0 && h <= array.length - 1 && w >= 0 && w <= array.length - 1) {
+      if (array[h][w] !== 9) {
+        array[h][w]++;
+      }
+    }
+  }
+
+  for (let h = 0; h < array.length; h++) {
+    for (let w = 0; w < array[0].length; w++) {
+      if (array[h][w] === 9) {
+        plusOne(h, w - 1);
+        plusOne(h, w + 1);
+        plusOne(h - 1, w);
+        plusOne(h + 1, w);
+        plusOne(h - 1, w - 1);
+        plusOne(h + 1, w + 1);
+        plusOne(h - 1, w + 1);
+        plusOne(h + 1, w - 1);
+      }
+    }
+  }
+
+  for (let h = 0; h < array.length; h++) {
+    for (let w = 0; w < array[0].length; w++) {
+      if (array[h][w] === 9) {
+        array[h][w] = 1;
+      }
+    }
+  }
+
+  return array;
 }
 
 module.exports = minesweeper;
